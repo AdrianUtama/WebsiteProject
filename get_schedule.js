2add20b027f272db110d1f7871e4d29dbc365453
Utam25@tamu.edu <utam25@build.tamu.edu>
2017-05-06 16:45:41 -0500

    change default

--- get_schedule.js
+++ get_schedule.js
@@ -38,7 +38,7 @@ function DoSomethingWithData(data) {
   }
   str += "</table>";
   if (counter == 0) {
-    str = "<strong>No Games Available</strong>";
+    str = "<font color = white><strong>No Games Available</strong></font>";
   }
   document.getElementById("sport").innerHTML = str;
 
